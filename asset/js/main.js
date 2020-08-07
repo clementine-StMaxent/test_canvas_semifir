5311138eb9704d59a873bbb670befa4c9e404212
clemsemifir <clementine@semifir.com>
2020-08-07 16:44:09 +0200

    rotation rectangle

--- asset/js/main.js
+++ asset/js/main.js
@@ -5,30 +5,35 @@ var x = canvas.width / 2;
 var y = canvas.height - 30;
 var dx = 2;
 var dy = -50;
-// var paddleHeight = 30;
-// var paddleWidth = 75;
-// var paddleX = (canvas.width - paddleWidth) / 2;
+
+
 
 function rectangle() {
     var ctx = canvas.getContext("2d");
-    /*
+    ctx.save();
+    ctx.translate(240, 120);
+    ctx.rotate(Math.PI / 4); // 45 degrees
+        /*
     1ère valeur : largeur dans l'espace
     2ème valeur : hauteur dans l'espace
     3ème valeur : largeur du rectangle
     4ème valeur : longueur du rectangle
-    */
-    ctx.rect(435, 750, 40, 150);
-    ctx.fillStyle = "black";
+    */    
+    ctx.fillRect(460, 20, 30, 150);
     ctx.fill();
+    ctx.restore();
 }
 
 function drawBall() {
     ctx.beginPath();
     //dessiner la ligne de la fonction drawBall() :
-    ctx.arc(x, y, 10, ballRadius, 0, Math.PI * 2);
-    ctx.fillStyle = "#0095DD";
+    ctx.arc(490, 490, 20, 0, ballRadius, 0, Math.PI * 2);
+    ctx.arc(x, y, 10, 2 * Math.PI, false);
+    // ctx.arc(100,100,20,0,2 * Math.PI);
+    ctx.fillStyle = "red";
     ctx.fill();
     ctx.closePath();
+
 }
 
 function drawPaddle() {
@@ -46,14 +51,17 @@ function draw() {
     drawBall();
     drawPaddle();
 
-//La balle ne ressort  plus
+
+    //La balle ne ressort  plus
     if (y + dy > canvas.height - ballRadius) {
         dy = -dy;
     }
 
     //nous avons juste mit la hauteur et non la largeur
     y += dy;
+
 }
+
 
 /*l'animation de la bille, j'ai diu la modifier en fonction de la ligne de la bille si je laissais la valeur
 setInterval(draw, 10) la bille allait trop vite
