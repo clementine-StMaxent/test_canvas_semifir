e1c7a61217e6b1968103e00bbb4fd4a4fc402583
clemsemifir <clementine@semifir.com>
2020-08-07 14:52:56 +0200

    modification rectangle et balle

--- asset/js/main.js
+++ asset/js/main.js
@@ -4,26 +4,33 @@ var ballRadius = 10;
 var x = canvas.width / 2;
 var y = canvas.height - 30;
 var dx = 2;
-var dy = -2;
+var dy = -50;
 // var paddleHeight = 30;
 // var paddleWidth = 75;
 // var paddleX = (canvas.width - paddleWidth) / 2;
 
 function rectangle() {
-    var canvas = document.getElementById("tutoriel");
     var ctx = canvas.getContext("2d");
-    ctx.rect(400, 500, 40, 150);
+    /*
+    1ère valeur : largeur dans l'espace
+    2ème valeur : hauteur dans l'espace
+    3ème valeur : largeur du rectangle
+    4ème valeur : longueur du rectangle
+    */
+    ctx.rect(435, 750, 40, 150);
     ctx.fillStyle = "black";
     ctx.fill();
 }
 
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+    //dessiner la ligne de la fonction drawBall() :
+    ctx.arc(x, y, 10, ballRadius, 0, Math.PI * 2);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
 }
+
 function drawPaddle() {
     ctx.beginPath();
     // ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
@@ -34,19 +41,21 @@ function drawPaddle() {
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    //nous déclarons les fonctions à afficher
     rectangle();
     drawBall();
     drawPaddle();
 
-    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
-        dx = -dx;
-    }
-    if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+//La balle ne ressort  plus
+    if (y + dy > canvas.height - ballRadius) {
         dy = -dy;
     }
 
-    x += dx;
+    //nous avons juste mit la hauteur et non la largeur
     y += dy;
 }
 
-setInterval(draw, 10);
+/*l'animation de la bille, j'ai diu la modifier en fonction de la ligne de la bille si je laissais la valeur
+setInterval(draw, 10) la bille allait trop vite
+*/
+setInterval(draw, 100);
