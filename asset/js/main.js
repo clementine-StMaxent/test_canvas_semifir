30561bb3c5fb8d34148a14d82d355f586ef80f25
clemsemifir <clementine@semifir.com>
2020-08-07 14:36:38 +0200

    first commit

---
+++ asset/js/main.js
@@ -0,0 +1,52 @@
+var canvas = document.getElementById("tutoriel");
+var ctx = canvas.getContext("2d");
+var ballRadius = 10;
+var x = canvas.width / 2;
+var y = canvas.height - 30;
+var dx = 2;
+var dy = -2;
+// var paddleHeight = 30;
+// var paddleWidth = 75;
+// var paddleX = (canvas.width - paddleWidth) / 2;
+
+function rectangle() {
+    var canvas = document.getElementById("tutoriel");
+    var ctx = canvas.getContext("2d");
+    ctx.rect(400, 500, 40, 150);
+    ctx.fillStyle = "black";
+    ctx.fill();
+}
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+function drawPaddle() {
+    ctx.beginPath();
+    // ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    rectangle();
+    drawBall();
+    drawPaddle();
+
+    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+    }
+
+    x += dx;
+    y += dy;
+}
+
+setInterval(draw, 10);
